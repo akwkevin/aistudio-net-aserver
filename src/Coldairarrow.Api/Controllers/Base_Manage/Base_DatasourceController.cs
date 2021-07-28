0dd5a3d61966aa795dc24f20dda2de372ac622f8
addy <398061334@qq.com>
2021-07-28 23:20:16 +0800

    快捷查询设置

---
+++ src/Coldairarrow.Api/Controllers/Base_Manage/Base_DatasourceController.cs
@@ -0,0 +1,65 @@
+﻿using Coldairarrow.Business.Base_Manage;
+using Coldairarrow.Entity.Base_Manage;
+using Coldairarrow.Util;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Coldairarrow.Api.Controllers.Base_Manage
+{
+    [Route("/Base_Manage/[controller]/[action]")]
+    public class Base_DatasourceController : BaseApiController
+    {
+        #region DI
+
+        public Base_DatasourceController(IBase_DatasourceBusiness base_DatasourceBus)
+        {
+            _base_DatasourceBus = base_DatasourceBus;
+        }
+
+        IBase_DatasourceBusiness _base_DatasourceBus { get; }
+
+        #endregion
+
+        #region 获取
+
+        [HttpPost]
+        public async Task<PageResult<Base_Datasource>> GetDataList(PageInput<ConditionDTO> input)
+        {
+            return await _base_DatasourceBus.GetDataListAsync(input);
+        }
+
+        [HttpPost]
+        public async Task<Base_Datasource> GetTheData(IdInputDTO input)
+        {
+            return await _base_DatasourceBus.GetTheDataAsync(input.id);
+        }
+
+        #endregion
+
+        #region 提交
+
+        [HttpPost]
+        public async Task SaveData(Base_Datasource data)
+        {
+            if (data.Id.IsNullOrEmpty())
+            {
+                InitEntity(data);
+
+                await _base_DatasourceBus.AddDataAsync(data);
+            }
+            else
+            {
+                await _base_DatasourceBus.UpdateDataAsync(data);
+            }
+        }
+
+        [HttpPost]
+        public async Task DeleteData(List<string> ids)
+        {
+            await _base_DatasourceBus.DeleteDataAsync(ids);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Coldairarrow.Business/Base_Manage/Base_DatasourceBusiness.cs
@@ -0,0 +1,65 @@
+﻿using Coldairarrow.Entity.Base_Manage;
+using Coldairarrow.Util;
+using EFCore.Sharding;
+using LinqKit;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+
+namespace Coldairarrow.Business.Base_Manage
+{
+    public class Base_DatasourceBusiness : BaseBusiness<Base_Datasource>, IBase_DatasourceBusiness, ITransientDependency
+    {
+        public Base_DatasourceBusiness(IDbAccessor db)
+            : base(db)
+        {
+        }
+
+        #region 外部接口
+
+        public async Task<PageResult<Base_Datasource>> GetDataListAsync(PageInput<ConditionDTO> input)
+        {
+            var q = GetIQueryable();
+            var where = LinqHelper.True<Base_Datasource>();
+            var search = input.Search;
+
+            //筛选
+            if (!search.condition.IsNullOrEmpty() && !search.keyword.IsNullOrEmpty())
+            {
+                var newWhere = DynamicExpressionParser.ParseLambda<Base_Datasource, bool>(
+                    ParsingConfig.Default, false, $@"{search.condition}.Contains(@0)", search.keyword);
+                where = where.And(newWhere);
+            }
+
+            return await q.Where(where).GetPageResultAsync(input);
+        }
+
+        public async Task<Base_Datasource> GetTheDataAsync(string id)
+        {
+            return await GetEntityAsync(id);
+        }
+
+        public async Task AddDataAsync(Base_Datasource data)
+        {
+            await InsertAsync(data);
+        }
+
+        public async Task UpdateDataAsync(Base_Datasource data)
+        {
+            await UpdateAsync(data);
+        }
+
+        public async Task DeleteDataAsync(List<string> ids)
+        {
+            await DeleteAsync(ids);
+        }
+
+        #endregion
+
+        #region 私有成员
+
+        #endregion
+    }
+}
